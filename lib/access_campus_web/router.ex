05c6a8dcebd524685ee3e21e66f4031ceddd9fdb
Eric Kim <erickim195@gmail.com>
2020-05-11 21:39:32 -0700

    changed api route to /

--- lib/access_campus_web/router.ex
+++ lib/access_campus_web/router.ex
@@ -2,32 +2,34 @@ defmodule AccessCampusWeb.Router do
   use AccessCampusWeb, :router
 
   pipeline :browser do
-    plug CORSPlug, origin: "*"
-    plug :accepts, ["html", "json"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(CORSPlug, origin: "*")
+    plug(:accepts, ["html", "json"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug CORSPlug, origin: "*"
-    plug :accepts, ["json"]
-    plug :fetch_session
-    plug :fetch_flash
+    plug(CORSPlug, origin: "*")
+    plug(:accepts, ["json"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
   end
 
   scope "/", AccessCampusWeb do
-    pipe_through :browser
+    pipe_through(:api)
 
-    get "/", PageController, :index
+    # get "/", PageController, :index
+    get("/", BuildingController, :index)
+    get("/:id", BuildingController, :show)
   end
 
   # Other scopes may use custom stacks.
-  scope "/api", AccessCampusWeb do
-    pipe_through :api
+  # scope "/api", AccessCampusWeb do
+  #   pipe_through :api
 
-    get "/buildings", BuildingController, :index
-    get "/buildings/:id", BuildingController, :show
-  end
+  #   get "/buildings", BuildingController, :index
+  #   get "/buildings/:id", BuildingController, :show
+  # end
 end
