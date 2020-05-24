3c87e82049d0e6c59ba5f8e589f7b2b2e171424f
Eric Kim <erickim195@gmail.com>
2020-05-24 11:27:18 -0700

    set debug errors to false

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :acccess_campus_api, AcccessCampusApi.Repo,
 # with webpack to recompile .js and .css sources.
 config :acccess_campus_api, AcccessCampusApiWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: []
