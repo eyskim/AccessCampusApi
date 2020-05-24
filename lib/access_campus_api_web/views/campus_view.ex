37c68c72b98d8ebda48698f40cb283da24ce8472
Eric Kim <erickim195@gmail.com>
2020-05-24 12:25:20 -0700

    added CampusView and have endpoint showing appropriate data

---
+++ lib/access_campus_api_web/views/campus_view.ex
@@ -0,0 +1,16 @@
+defmodule AccessCampusApiWeb.CampusView do
+  use AccessCampusApiWeb, :view
+  alias AccessCampusApiWeb.CampusView
+
+  def render("index.json", %{campuses: campuses}) do
+    %{data: render_many(campuses, CampusView, "campus.json")}
+  end
+
+  def render("show.json", %{campus: campus}) do
+    %{data: render_one(campus, CampusView, "campus.json")}
+  end
+
+  def render("campus.json", %{campus: campus}) do
+    %{id: campus.id, name: campus.name, color: campus.theme_color, coords: campus.coords}
+  end
+end
