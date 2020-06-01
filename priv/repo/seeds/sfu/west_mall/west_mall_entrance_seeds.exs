afbdc3b74222504e3a9f6652c5815ebc646931ef
Eric Kim <erickim195@gmail.com>
2020-05-31 23:34:27 -0700

    added WMC at SFU entrances

---
+++ priv/repo/seeds/sfu/west_mall/west_mall_entrance_seeds.exs
@@ -0,0 +1,33 @@
+alias AccessCampusApi.Repo
+alias AccessCampusApi.Entrances.Entrance
+  
+Repo.insert! %Entrance{
+    coords: %{lat: 49.279738, lng: -122.922657},
+    building_id: 6,
+    building_name: "West Mall Centre"
+}
+
+Repo.insert! %Entrance{
+    coords: %{lat: 49.279994, lng: -122.922549},
+    building_id: 6,
+    building_name: "West Mall Centre"
+}
+ 
+Repo.insert! %Entrance{
+    coords: %{lat: 49.279775, lng: -122.920948},
+    building_id: 6,
+    building_name: "West Mall Centre"
+}
+
+Repo.insert! %Entrance{
+    coords: %{lat: 49.279493, lng: -122.921034},
+    building_id: 6,
+    building_name: "West Mall Centre"
+}
+
+Repo.insert! %Entrance{
+    coords: %{lat: 49.279819, lng: -122.921809},
+    building_id: 6,
+    building_name: "West Mall Centre"
+}
+
