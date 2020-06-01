ba6663525b2246f074617b165240a1b724ebff01
Eric Kim <erickim195@gmail.com>
2020-06-01 10:09:37 -0700

    adjusted seeds for harvard and googleplex

--- priv/repo/seeds/googleplex/googleplex/googleplex_entrance_seeds.exs
+++ priv/repo/seeds/googleplex/googleplex/googleplex_entrance_seeds.exs
@@ -3,25 +3,25 @@ alias AccessCampusApi.Entrances.Entrance
   
 Repo.insert! %Entrance{
     coords: %{lat: 37.4218933, lng: -122.0839232},
-    building_id: 12,
+    building_id: 11,
     building_name: "Googleplex"
 }
 
 Repo.insert! %Entrance{
     coords: %{lat: 37.4219019, lng: -122.08419},
-    building_id: 12,
+    building_id: 11,
     building_name: "Googleplex"
 }
 
 Repo.insert! %Entrance{
     coords: %{lat: 37.42199, lng: -122.0843319},
-    building_id: 12,
+    building_id: 11,
     building_name: "Googleplex"
 }
 
 Repo.insert! %Entrance{
     coords: %{lat: 37.4220528, lng: -122.0840105},
-    building_id: 12,
+    building_id: 11,
     building_name: "Googleplex"
 }
 
--- priv/repo/seeds/harvard/harvard_hall/harvard_hall_entrance_seeds.exs
+++ priv/repo/seeds/harvard/harvard_hall/harvard_hall_entrance_seeds.exs
@@ -3,7 +3,7 @@ alias AccessCampusApi.Entrances.Entrance
   
 Repo.insert! %Entrance{
     coords: %{lat: 42.374946, lng: -71.118163},
-    building_id: 10,
+    building_id: 9,
     building_name: "Harvard Hall"
 }
 
